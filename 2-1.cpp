05557bc60d6c6267a4b7ef3647d5bdaa9ab9a5c5
Roman Semenovich <rsemenovich@live.com>
2016-10-26 20:35:39 +0300

    homework

---
+++ 2-1.cpp
@@ -0,0 +1,51 @@
+//выводит "перевернутый" массив
+#include <QCoreApplication>
+#include <iostream>
+#include <math.h>
+#include <ctime>
+
+using namespace std;
+
+void createArray(int Arr[], int N);
+void printArray(int Arr[], int N);
+void turnArray(int Arr[], int N);
+
+int main(){
+    srand(time(NULL));
+    int N;
+    cout<<"Enter the size of the array: ";
+    cin>>N;
+    int Arr[N];
+    createArray(Arr, N);
+    cout<<"The array: ";
+    printArray(Arr, N);
+    cout<<"The inverse array: ";
+    turnArray(Arr, N);
+    printArray(Arr, N);
+}
+
+void createArray(int Arr[], int N){
+    int i = 0;
+    while (i < N){
+        Arr[i] = (rand() % (2*N + 1)) - N;
+        i++;
+    }
+}
+
+void printArray(int Arr[], int N){
+    int i = 0;
+    while (i < N){
+        cout<<Arr[i]<<" ";
+        i++;
+    }
+    cout<<"\n";
+}
+
+void turnArray(int Arr[], int N){
+    int i = 0, j = N - 1;
+    while (i < N/2){
+        swap(Arr[i], Arr[j]);
+        i++;
+        j--;
+    }
+}
