c8fab85d5dede1e0fe62892ac4a1092473306aef
Roman <rsemenovich@live.com>
2017-03-01 19:19:22 +0300

    homework

---
+++ 2-1.cpp
@@ -0,0 +1,30 @@
+//Линейный конгруэнтный генератор псевдослучайных чисел
+
+#include <QCoreApplication>
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int rand0m();
+
+int main()
+{
+    int N;
+    cout << "Введите количество случайных чисел: ";
+    cin >> N;
+    for (int i = 0; i < N; i++){
+        cout << "Случайное число " << i + 1 << ": " <<  rand0m() << "\n";
+    }
+}
+
+int rand0m(){
+    static int previous = time(NULL) % 1000;
+    int next;
+    //int a = 4096, c = 150889, m = 714025; // (a)
+    int a = 796, c = 5152, m = 64395; // (b)
+    next = (a * previous + c) % m;
+    previous = next;
+    return next;
+}
+
