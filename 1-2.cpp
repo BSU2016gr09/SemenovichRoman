fb033229ebaebc1e9bca3ee92417f37c5b931596
Roman <rsemenovich@live.com>
2017-03-01 18:55:37 +0300

    homework

--- 1-2.cpp
+++ 1-2.cpp
@@ -1,12 +1,19 @@
+/*Даны точки плоскости своими координатами в виде двух одномерных массивов.
+  Точки плоскости рассортировать по возрастанию расстояния до прямой ax + by + c = 0. */
+
 #include <QCoreApplication>
 #include <iostream>
 #include <math.h>
 
 using namespace std;
 
-void inputPoints(char * Points, int * X, int * Y, int N);
-void printPoints(char * Points, int * X, int * Y, int N);
-void sortPoints(char * Points, int a, int b, int c, int * X, int * Y, int N);
+void inputPoints(char * Points, int * X, int * Y, int size);
+void printPoints(char * Points, int * X, int * Y, int size);
+void sortPoints(char * Points, int a, int b, int c, int * X, int * Y, int size);
+void giveMemory(int * &Array, int size);
+void giveMemory(char * &Array, int size);
+void deleteMemory (int * &Array);
+void deleteMemory (char * &Array);
 
 int main()
 {
@@ -23,14 +30,18 @@ int main()
     cout << "\n" << a << "x + " << b << "y + " << c << " = 0";
     cout << "\nВведите количество точек на плоскости: ";
     cin >> N;
-    char * Points = new char [N]; //Где выделение и контроль динамической памяти??? Почему выделение не в отдельной функции???
-    int * X = new int[N]; //Где выделение и контроль динамической памяти??? Почему выделение не в отдельной функции???
-    int * Y = new int[N];//Где выделение и контроль динамической памяти??? Почему выделение не в отдельной функции???
+    char * Points;
+    int * X;
+    int * Y;
+    giveMemory(Points, N);
+    giveMemory(X, N);
+    giveMemory(Y, N);
     inputPoints(Points, X, Y, N);
     sortPoints(Points, a, b, c, X, Y, N);
     printPoints(Points, X, Y, N);
-    delete [] Y;
-    delete [] X;
+    deleteMemory(Points);
+    deleteMemory(X);
+    deleteMemory(Y);
     Points = nullptr;
     X = nullptr;
     Y = nullptr;
@@ -38,39 +49,69 @@ int main()
     return 0;
 }
 
-void inputPoints(char * Points, int *X, int *Y, int N){
-    for (int i = 0; i < N; i++){
-        Points[i] = 'A' + char(i);
+void giveMemory(int * &Array, int size){
+    try{
+        Array = new int [size];
     }
-    for (int i = 0; i < N; i++){
-        cout << "\nВведите координату X точки " << Points[i] << ": ";
-        cin >> X[i];
-        cout << "\nВведите координату Y точки " << Points[i] << ": ";
-        cin >> Y[i];
+    catch(...){
+        cout << "Память не выделилась :(";
+        exit(0);
     }
 }
 
-void printPoints(char * Points, int *X, int *Y, int N){
-    for (int i = 0; i < N; i++){
-        cout << Points[i] << "(" << X[i] << "; " << Y[i] << "), ";
+void deleteMemory (int * &Array){
+    delete [] Array;
+}
+
+void giveMemory(char * &Array, int size){
+    try{
+        Array = new char [size];
+    }
+    catch(...){
+        cout << "Память не выделилась :(";
+        exit(0);
     }
 }
 
-void sortPoints(char * Points, int a, int b, int c, int * X, int * Y, int N){
-    int * Distance = new int[N];
-    for (int i = 0; i < N; i++){
-        Distance[i] = abs(a * X[i] + b * Y[i] + c) / sqrt(a * a + b * b);
+void deleteMemory (char * &Array){
+    delete [] Array;
+}
+
+void inputPoints(char * Points, int *X, int *Y, int size){
+    for (int i = 0; i < size; i++){
+       *(Points + i) = 'A' + char(i);
     }
-    for (int i = 0; i < N - 1; i++){
-        for (int j = 0; j < N - 1; j++){
-            if (Distance[j] > Distance[j + 1]){
-                swap(Distance[j], Distance[j + 1]);
-                swap(X[j], X[j + 1]);
-                swap(Y[j], Y[j + 1]);
-                swap(Points[j], Points[j + 1]);
+    for (int i = 0; i < size; i++){
+        cout << "\nВведите координату X точки " << *(Points + i) << ": ";
+        cin >> *(X + i);
+        cout << "\nВведите координату Y точки " << *(Points + i) << ": ";
+        cin >> *(Y + i);
+    }
+}
+
+void printPoints(char * Points, int *X, int *Y, int size){
+    for (int i = 0; i < size; i++){
+        cout << *(Points + i) << "(" << *(X + i) << "; " << *(Y + i) << "), ";
+    }
+}
+
+void sortPoints(char * Points, int a, int b, int c, int * X, int * Y, int size){
+    int * Distance;
+    giveMemory(Distance, size);
+    for (int i = 0; i < size; i++){
+        *(Distance + i) = abs(a * *(X + i) + b * *(Y + i) + c) / sqrt(a * a + b * b);
+    }
+    for (int i = 0; i < size - 1; i++){
+        for (int j = 0; j < size - 1; j++){
+            if (*(Distance + j) > *(Distance + j + 1)){
+                swap(*(Distance + j), *(Distance + j + 1));
+                swap(*(X + j), *(X + j + 1));
+                swap(*(Y + j), *(Y + j + 1));
+                swap(*(Points + j), *(Points + j + 1));
             }
         }
     }
-    delete [] Distance;
+    deleteMemory(Distance);
     Distance = nullptr;
 }
+
---
+++ 2-2.cpp
@@ -0,0 +1,30 @@
+//XORShift
+
+#include <QCoreApplication>
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int rand0m();
+
+int main()
+{
+    int N;
+    cout << "Введите количество случайных чисел: ";
+    cin >> N;
+    for (int i = 0; i < N; i++){
+        cout << "Случайное число " << i + 1 << ": " <<  rand0m() << "\n";
+    }
+}
+
+int rand0m(){
+    static int number = time(NULL) % 1000;
+    int a = 13;
+    int b = 15;
+    int c = 5;
+    number ^= number << a;
+    number ^= number >> b;
+    number ^= number << c;
+    return number;
+}
