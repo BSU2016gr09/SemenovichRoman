d5a9c67b726678bfe248fb52cb32e27e6aa67148
Roman <rsemenovich@live.com>
2017-03-23 23:14:51 +0300

    integral

--- integral.cpp
+++ integral.cpp
@@ -9,6 +9,8 @@ typedef double (*pf)(double);
 double mySin(double);
 double x2(double);
 double reverseX(double);
+double reverse1PlusX2(double);
+double reverseSin2(double);
 double calcIntegral(int, int, double (*pf)(double), double);
 
 int main()
@@ -16,6 +18,7 @@ int main()
     double a;
     double b;
     double epsilon;
+    pf integral[] = {x2, mySin, reverseX, reverseSin2, reverse1PlusX2};
     cout << "Введите a: ";
     cin >> a;
     cout << "Введите b: ";
@@ -23,23 +26,17 @@ int main()
     cout << "Введите эпсилон: ";
     cin >> epsilon;
     int choise;
-    cout << "От какой функции возьмем интеграл? 1. x^2; 2.sinx, 3.1/x: ";
+    cout << "От какой функции возьмем интеграл? 1. x^2; 2.sinx; 3.1/x; 4. 1/sin^2; 5. 1/(1 + x^2): ";
     cin >> choise;
-    // добавь еще таких подинтегральных функций. Засунь их все в массив. И потом проси пользователя ввести НОМЕР функции. После этого вызывай расчет
-    /*switch (choise) {
-    case 1:
-        cout << calcIntegral(a, b, x2, epsilon) << "\n";
-        break;
-    case 2:
-        cout << calcIntegral(a, b, mySin, epsilon) << "\n";
-        break;
-    case 3:
-        cout << calcIntegral(a, b, reverseX, epsilon) << "\n";
-        break;
-    default:
-        break;
-    }*/
+    cout << "Интеграл: " << calcIntegral(a, b, *(integral[choise]), epsilon) << "\n";
+}
 
+double reverse1PlusX2(double x){
+    return 1/(1 + x*x);
+}
+
+double reverseSin2(double x){
+    return 1/sin(x)*sin(x);
 }
 
 double mySin(double x){
@@ -59,18 +56,16 @@ double calcIntegral(int a, int b, double (*pf)(double), double epsilon){
     double x;
     double l;
     double sum = 0, oldsum = 0;
-    double rez;
     do{
         oldsum = sum;
+        sum = 0;
         l = (double)(b - a) / N;
         x = a;
-        while(x <= b){//зачем тут =? У нас же метод ЛЕВЫХ прямоугольников!!!
-            rez = pf(x);
-            if (rez < 0) rez = -rez;//неверно!!!!!!
+        while(x < b){
             x = x + l;
-            sum = l * rez;//не посчитает!!!! Пропущен +
+            sum = sum + (l * fabs(pf(x)));
         }
-        N += N/2;
+        N += N*2;
     }
     while ((sum - oldsum) > epsilon);
     return sum;
