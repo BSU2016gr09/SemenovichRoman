d20de9bb5016ff4082f7d5612db1e8e2f77b5e56
barvenov <barvenovsa@gmail.com>
2017-03-23 11:48:15 +0300

    Ошибки+Задание

--- integral.cpp
+++ integral.cpp
@@ -25,7 +25,8 @@ int main()
     int choise;
     cout << "От какой функции возьмем интеграл? 1. x^2; 2.sinx, 3.1/x: ";
     cin >> choise;
-    switch (choise) {
+    // добавь еще таких подинтегральных функций. Засунь их все в массив. И потом проси пользователя ввести НОМЕР функции. После этого вызывай расчет
+    /*switch (choise) {
     case 1:
         cout << calcIntegral(a, b, x2, epsilon) << "\n";
         break;
@@ -37,7 +38,7 @@ int main()
         break;
     default:
         break;
-    }
+    }*/
 
 }
 
@@ -63,11 +64,11 @@ double calcIntegral(int a, int b, double (*pf)(double), double epsilon){
         oldsum = sum;
         l = (double)(b - a) / N;
         x = a;
-        while(x <= b){
+        while(x <= b){//зачем тут =? У нас же метод ЛЕВЫХ прямоугольников!!!
             rez = pf(x);
-            if (rez < 0) rez = -rez;
+            if (rez < 0) rez = -rez;//неверно!!!!!!
             x = x + l;
-            sum = l * rez;
+            sum = l * rez;//не посчитает!!!! Пропущен +
         }
         N += N/2;
     }
