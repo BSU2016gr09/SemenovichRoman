c84976adfd0b1c053913edc8c1e272f8829a7197
barvenov <barvenovsa@gmail.com>
2017-04-07 09:59:05 +0300

    доделать...

--- quadratic.cpp
+++ quadratic.cpp
@@ -11,12 +11,13 @@ class Quadratic{
         double a;
         double b;
         double c;
+    //нет конструкторов...
         void calcDiscriminant(){
             discriminant = b * b - 4 * a * c;
             cout << "Дискриминант уравнения равен " << discriminant << "\n";
 
         }
-        void calcRoots(){
+        void calcRoots(){//А откуда эта функция знает про значение дискриминанта? Вдруг он еще не вычислен!!!!
             if (discriminant < 0){
                 cout << "Уравнение не имеет корней :( \n";
                 return;
@@ -56,7 +57,7 @@ int main(){
     q.b = 9;
     q.c = -1;
     q.printQuadratic();
-    q.calcDiscriminant();
+    q.calcDiscriminant();//По моему неудачная реализация. Пользователь вообще не должен знать про дискриминант. А может он по другим формулам считал всегда!!!
     q.calcRoots();
     w = q;
     e = q.sum(w);
